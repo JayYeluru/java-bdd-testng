a9d64dc4b0d720a3334c56b342a10f0538f78922
yjkn <yjkn@hotmail.com>
2019-09-20 16:49:50 +0200

    Initial commit

---
+++ src/main/java/java-bdd-testng/App.java
@@ -0,0 +1,14 @@
+package java-bdd-testng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
